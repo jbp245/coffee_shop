0992c774c11c56dd0b9dc7d25396de949ac7e455
jbp245 <jbp245@cornell.edu>
2021-11-07 16:27:29 -0500

    structs of products to offer

---
+++ data/products.go
@@ -0,0 +1,39 @@
+package data
+
+import "time"
+
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
+	DeletedON   string
+}
+
+func GetProducts() []*Product {
+	return productList
+}
+
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc123",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Fine coffee extracted under pressure",
+		Price:       1.99,
+		SKU:         "xyz789",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
